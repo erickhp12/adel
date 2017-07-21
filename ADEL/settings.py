f2a9470c167a8d59fa0fcaae0b32123270b6cf4e
Francisco Trejo <trejo@Franciscos-MacBook-Air.local>
2017-07-21 00:01:43 -0600

    cambio en statics

--- ADEL/settings.py
+++ ADEL/settings.py
@@ -143,10 +143,11 @@ DIVISA = 18
 #     os.path.join(BASE_DIR, "static"),
 # ]
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_DIR, "static"),
 )
 STATIC_URL = '/static/'
 
