6d96687bd8ed6832c6dac5ad72f5b3a3828e04d6
Francisco Trejo <trejo@Franciscos-MacBook-Air.local>
2017-07-25 12:49:38 -0600

    configuracion de master a server:

--- ADEL/settings.py
+++ ADEL/settings.py
@@ -17,9 +17,6 @@ LOCAL = False
 ALLOWED_HOSTS = ['*']
 
 
-SITE_ID = 1
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,35 +35,23 @@ INSTALLED_APPS = [
     'agenda'
 ]
 
-MIDDLEWARE_CLASSES = (
+SITE_ID = 1
+
+MIDDLEWARE_CLASSES = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+]
 
 ROOT_URLCONF = 'ADEL.urls'
 
-TEMPLATES = [
-   {
-       'BACKEND': 'django.template.backends.django.DjangoTemplates',
-       'DIRS': [
-           os.path.join(BASE_DIR, 'templates'),
-           os.path.join(BASE_DIR, 'ADEL/templates')
-       ],
-       'APP_DIRS': True,
-       'OPTIONS': {
-           'context_processors': [
-               'django.template.context_processors.debug',
-               'django.template.context_processors.request',
-               'django.contrib.auth.context_processors.auth',
-               'django.contrib.messages.context_processors.messages',
-           ],
-       },
-   },
-]
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+    os.path.join(BASE_DIR, 'ADEL/templates')
+)
 
 WSGI_APPLICATION = 'ADEL.wsgi.application'
 
@@ -80,6 +65,17 @@ DATABASES = {
         'PORT': 3306
     }
 }
+
+# Cache
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -113,21 +109,13 @@ USE_TZ = True
 DIVISA = 18
 
 # Static files (CSS, JavaScript, Images)
-# STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
-# STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
 
 MEDIA_URL = '/static/media/'
 
