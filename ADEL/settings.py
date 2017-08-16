c33d8f1996c31998814071dd902aefc7874c9b28
Francisco Trejo <trejo@Franciscos-MacBook-Air.local>
2017-08-16 01:38:43 -0600

    settings

--- ADEL/settings.py
+++ ADEL/settings.py
@@ -142,7 +142,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "/static/"),
+    os.path.join(BASE_DIR, "/static"),
 ]
 
 
