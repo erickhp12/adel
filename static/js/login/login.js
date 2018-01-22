acece4834df308553069391741753d11f114977d
NEWUSER <erick.pardavell@ventamovil.com.mx>
2018-01-22 09:10:31 -0700

    nuevo login

--- static/js/login/login.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $('<img/>').attr('src', '../static/img/iconos/inicio.jpg').load(function() {
-    $('.bg-img').append($(this));
-    // simulate loading  
-    $('.container').addClass('loaded');
-  });
-})
